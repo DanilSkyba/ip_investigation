4297f0c19da26b713ebafa5d8eb5dd3485f0b407
Данило Скиба <skyba.dm@WSH57006.np.work>
2023-12-26 16:18:35 +0200

    Update app.py and index.html

--- app.py
+++ app.py
@@ -1,40 +1,4 @@
 '''
-from flask import Flask, request, render_template, redirect, url_for
-
-app = Flask(__name__)
-
-@app.get('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/', methods=['POST', 'GET'])
-def add():
-    if request.method == 'POST':
-        print(request.form("inputs"))
-        return redirect(url_for('home'))
-
-'''
-'''
-from flask import Flask, request, render_template, redirect, url_for
-
-app = Flask(__name__)
-
-@app.route('/', methods=['POST', 'GET'])
-def home():
-    try:
-        if request.method == 'POST':
-            print("Form Data:", request.form)
-            return redirect(url_for('home'))
-        return render_template('index.html')
-    except Exception as e:
-        print("Error:", str(e))
-        return "Internal Server Error", 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
-'''
-
-'''
 Doesn't work in AWS App Runner
 from flask import Flask, request, render_template, redirect, url_for
 import webbrowser
@@ -74,7 +38,6 @@ if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
 '''
 from flask import Flask, request, render_template
-import webbrowser
 
 app = Flask(__name__)
 
@@ -86,23 +49,18 @@ def home():
 
             # Build URLs based on user input
             urls = [
-                "https://www.talosintelligence.com/reputation_center/lookup?search={0}".format(user_input),
-                "https://www.virustotal.com/gui/search/{0}".format(user_input),
-                "https://www.abuseipdb.com/check/{0}".format(user_input),
-                "https://otx.alienvault.com/browse/global/pulses?q={0}&include_inactive=0&sort=-modified&page=1&limit=10&indicatorsSearch={0}".format(user_input)
+                {"name": "Talos Intelligence", "url": f"https://www.talosintelligence.com/reputation_center/lookup?search={user_input}"},
+                {"name": "VirusTotal", "url": f"https://www.virustotal.com/gui/search/{user_input}"},
+                {"name": "AbuseIPDB", "url": f"https://www.abuseipdb.com/check/{user_input}"},
+                {"name": "AlienVault OTX", "url": f"https://otx.alienvault.com/browse/global/pulses?q={user_input}&include_inactive=0&sort=-modified&page=1&limit=10&indicatorsSearch={user_input}"}
             ]
 
-            # Open the URLs in the web browser
-            open_browser_for_investigation(urls)
+            return render_template('index.html', urls=urls)
 
         return render_template('index.html')
     except Exception as e:
         print("Error:", str(e))
         return "Internal Server Error", 500
-
-def open_browser_for_investigation(urls):
-    for url in urls:
-        webbrowser.open(url, new=2)  # new=2 opens in a new tab
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
